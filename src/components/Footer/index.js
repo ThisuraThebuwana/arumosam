c1e40c20f2a5cb40070b6c08f1f2c74e2c332820
ThisuraThebuwana <thisura.thebuwana@gmail.com>
2021-09-12 18:36:03 +0530

    correct typo error

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -10,16 +10,16 @@ const Footer = () => {
                     <SocialMediaWrap>
                         <SocialLogo to='/'>Pizza</SocialLogo>
                         <SocialIcons>
-                            <SocialIconsLink href="/" target="_blank" aria-lable="Facebook" rel="noopener noreferrer">
+                            <SocialIconsLink href="/" target="_blank" aria-label="Facebook" rel="noopener noreferrer">
                                 <FaFacebook />
                             </SocialIconsLink>
-                            <SocialIconsLink href="/" target="_blank" aria-lable="Instagram" rel="noopener noreferrer">
+                            <SocialIconsLink href="/" target="_blank" aria-label="Instagram" rel="noopener noreferrer">
                                 <FaInstagram />
                             </SocialIconsLink>
-                            <SocialIconsLink href="/" target="_blank" aria-lable="Youtube" rel="noopener noreferrer">
+                            <SocialIconsLink href="/" target="_blank" aria-label="Youtube" rel="noopener noreferrer">
                                 <FaYoutube />
                             </SocialIconsLink>
-                            <SocialIconsLink href="/" target="_blank" aria-lable="Twitter" rel="noopener noreferrer">
+                            <SocialIconsLink href="/" target="_blank" aria-label="Twitter" rel="noopener noreferrer">
                                 <FaWhatsapp />
                             </SocialIconsLink>
                         </SocialIcons>
